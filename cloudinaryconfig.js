f2b27fbfc7d3a9465d3126eed8a9dc6426259d9b
OZO Gold and Diamonds <info@ozogoldanddiamonds.com>
2026-09-15 11:38:06 +0530

    udpate .env file and cloudinary keys

--- cloudinaryconfig.js
+++ cloudinaryconfig.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: 'dhi7e8a6l',
-  api_key: '337192569873694',
+  cloud_name: 'ivs9dcvf',
+  api_key: '962215883736861',
   api_secret: '1euG3rlJ7Le-M0bBeKWNoFcfMYw' // Click 'View API Keys' above to copy your API secret
 });
 
